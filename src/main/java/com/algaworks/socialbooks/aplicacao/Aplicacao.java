a49b7b01e9eea1055dfa6c06141c2f6cfccd0986
Andrews <limaandrews@msn.com>
2017-07-04 00:50:25 -0300

    ajustes do commite anterior

--- src/main/java/com/algaworks/socialbooks/aplicacao/Aplicacao.java
+++ src/main/java/com/algaworks/socialbooks/aplicacao/Aplicacao.java
@@ -1,9 +1,38 @@
 package com.algaworks.socialbooks.aplicacao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import com.algaworks.socialbooks.client.LivrosClient;
+import com.algaworks.socialbooks.domain.Livros;
+
 public class Aplicacao {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
+
+		LivrosClient client = new LivrosClient();
+
+		List<Livros> listaLivros = client.listar();
+
+		for (Livros livro : listaLivros) {
+
+			System.out.println("Livro: " + livro.getNome());
+
+		}
+
+		Livros livro = new Livros();
+
+		livro.setNome("Curso Rest Aplicado");
+
+		SimpleDateFormat publicacao = new SimpleDateFormat("dd/MM/yyyy");
+		livro.setPublicacao(publicacao.parse("16/10/1983"));
+		livro.setResumo("Muito bom o livro");
+		livro.setEditora("AlgaWorks");
+
+		String localization = client.Salvar(livro);
+
+		System.out.println(localization);
 
 	}
 
